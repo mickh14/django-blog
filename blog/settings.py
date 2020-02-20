6f2948ee4d459e5081f3329bcd89432323072785
mickh14 <michael.hurley@expleogroup.com>
2020-02-20 16:57:05 +0000

    update Procfile

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blog-test-app-mick.herokuapp.com']
+ALLOWED_HOSTS = ['*', 'blog-test-app-mick.herokuapp.com']
 
 
 # Application definition
